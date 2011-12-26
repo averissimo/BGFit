3bd0aae4e6a784c873e361bd7ec55ddb63713728
Andre Verissimo <averissimo@kdbio.inesc-id.pt>
2011-12-26 13:52:18 +0000

    hello world with mysql db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BacteriaGrowth::Application.routes.draw do
+  get "home/index"
+
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
