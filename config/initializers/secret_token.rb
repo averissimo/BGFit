76a46eb3430da5a52c2c0e63bb1c4c4a05f33602
Andre Verissimo <averissimo@kdbio.inesc-id.pt>
2011-12-26 12:52:01 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BacteriaGrowth::Application.config.secret_token = 'f954e34eac3f6adf682284830c18af1dde7013918f21bd065c10ef5a67ec3520a8f462e791da08e75bdaedb9ba0fb2fbed118e1836fae042a73d1cc8653f7f37'
