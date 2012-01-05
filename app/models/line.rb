cd7fab1ad0df2f316e7f687d4896ba2d7364865c
Andre Verissimo <averissimo@kdbio.inesc-id.pt>
2012-01-05 18:00:20 +0000

    fixes bug when y is 0 or nil in lines

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,6 +1,14 @@
 class Line < ActiveRecord::Base
   belongs_to :measurement
   
+  def formatted(input)
+    if input == nil
+      'null'
+    else
+      input
+    end
+  end
+  
   def z_formatted
       if z == nil
         'null'
