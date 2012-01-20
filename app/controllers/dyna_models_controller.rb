5e12965fd8698b17f7705ff432a977a981a4f81f
Andre Verissimo <averissimo@kdbio.inesc-id.pt>
2012-01-20 17:31:22 +0000

    adds dyna model controllers and views

---
+++ app/controllers/dyna_models_controller.rb
@@ -0,0 +1,58 @@
+class DynaModelsController < ApplicationController
+  respond_to :html, :json
+  
+  def index
+    @dyna_models = DynaModel.all
+    respond_with(@dyna_models)
+  end
+
+  def new
+    @dyna_model = DynaModel.new
+    respond_with @dyna_model
+  end
+
+  def create
+    @dyna_model = DynaModel.new(params[:dyna_model])
+    respond_with @dyna_model do | format |
+      if @dyna_model.save
+        flash[:notice] = t('flash.actions.create.notice', :resource_name => "Dyna Model")
+      else
+        format.html { render action: "new" }
+        format.json { render json: @dyna_model.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  
+  def edit
+    @dyna_model = DynaModel.find(params[:id])
+    respond_with(@dyna_models)
+  end
+
+  def update
+    @dyna_model = DynaModel.find(params[:id])
+
+    respond_with @dyna_model do |format|
+      if @dyna_model.update_attributes(params[:dyna_model])
+        flash[:notice] = t('flash.actions.update.notice', :resource_name => "Dyna Model")
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @dyna_model.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+  
+  def show
+    @dyna_model = DynaModel.find(params[:id])
+    respond_with(@dyna_models)
+  end
+
+  def destroy
+    @dyna_model = DynaModel.find(params[:id])
+    @dyna_model.destroy
+    respond_with(@dyna_model, :location => dyna_models_path)
+
+  end
+
+end
--- app/models/dyna_model.rb
+++ app/models/dyna_model.rb
@@ -4,4 +4,12 @@ class DynaModel < ActiveRecord::Base
   
   validates_uniqueness_of :title
 
+  public
+    def description_trimmed
+        if description.length > 97
+          return description[0..97] + "..." 
+        else
+          return description
+        end
+      end
 end
