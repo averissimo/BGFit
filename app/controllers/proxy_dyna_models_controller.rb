fe9b4f7e12d8a0c9234117306f4d82108d3be2ca
Andre Verissimo <averissimo@kdbio.inesc-id.pt>
2012-01-24 16:33:15 +0000

    adds proxy dyna model to measurement
    it is prepared to be compatible as dependant of experiments

--- app/controllers/proxy_dyna_models_controller.rb
+++ app/controllers/proxy_dyna_models_controller.rb
@@ -1,43 +1,43 @@
 class ProxyDynaModelsController < ApplicationController
   respond_to :html, :json
   
+  before_filter :determine_models , :except =>  [:index, :new, :create]
+  
   def index
-    @model = Model.find(params[:model_id])
-    @experiment = @model.experiments.find(params[:experiment_id])
-    @measurement = @experiment.measurements.find(params[:measurement_id])
-    redirect_to [@model,@experiment,@measurement]
+    @measurement = Measurement.find(params[:measurement_id])
+    @experiment = @measurement.experiment
+    @model = @experiment.model
+    @proxy_dyna_models = @measurement.proxy_dyna_models
+    redirect_to [@experiment,@measurement]
   end
 
   def new
-    @model = Model.find(params[:model_id])
-    if params[:measurement_id].nil?
-      @experiment = @model.experiments.find(params[:experiment_id])
+    if params[:experiment_id]
+      @experiment = Experiment.find(params[:experiment_id])
       @proxy_dyna_model = @experiment.proxy_dyna_models.build
-      @proxy_dyna_model.experiment = @experiment 
-    else
-      @experiment = @model.experiments.find(params[:experiment_id]) 
-      @measurement = @experiment.measurements.find(params[:measurement_id])
+    elsif params[:measurement_id]
+      @measurement = Measurement.find(params[:measurement_id])
+      @experiment = @measurement.experiment
       @proxy_dyna_model = @measurement.proxy_dyna_models.build
-      @proxy_dyna_model.measurement = @measurement
     end
+    @model = @experiment.model
     
     respond_with @proxy_dyna_model
   end
 
   def create
-    @model = Model.find(params[:model_id])
-    @experiment = @model.experiments.find(params[:experiment_id])
-    if params[:measurement_id].nil?
+    if params[:experiment_id]
+      @experiment = Experiment.find(params[:experiment_id])
       @proxy_dyna_model = @experiment.proxy_dyna_models.build(params[:dyna_model])
-      #@proxy_dyna_model.experiment = @experiment 
-    else
-      @measurement = @experiment.measurements.find(params[:measurement_id])
+    elsif params[:measurement_id]
+      @measurement = Measurement.find(params[:measurement_id])
+      @experiment = @measurement.experiment
       @proxy_dyna_model = @measurement.proxy_dyna_models.build(params[:dyna_model])
-      #@proxy_dyna_model.measurement = @measurement
     end
-
+    @model = @experiment.model
+  
     #@proxy_dyna_model = ProxyDynaModel.new(params[:dyna_model])
-    respond_with [@model,@experiment,@measurement,@proxy_dyna_model] do | format |
+    respond_with @proxy_dyna_model do | format |
       if @proxy_dyna_model.save
         flash[:notice] = t('flash.actions.create.notice', :resource_name => "Proxy Dyna Model")
       else
@@ -49,34 +49,43 @@ class ProxyDynaModelsController < ApplicationController
   
   
   def edit
-    @dyna_model = DynaModel.find(params[:id])
-    respond_with(@dyna_models)
+    respond_with(@proxy_dyna_model)
   end
 
   def update
-    @dyna_model = DynaModel.find(params[:id])
-
-    respond_with @dyna_model do |format|
-      if @dyna_model.update_attributes(params[:dyna_model])
-        flash[:notice] = t('flash.actions.update.notice', :resource_name => "Dyna Model")
+    respond_with @proxy_dyna_model do |format|
+      if @proxy_dyna_model.update_attributes(params[:dyna_model])
+        flash[:notice] = t('flash.actions.update.notice', :resource_name => "Proxy Dyna Model")
       else
         format.html { render action: "edit" }
-        format.json { render json: @dyna_model.errors, status: :unprocessable_entity }
+        format.json { render json: @proxy_dyna_model.errors, status: :unprocessable_entity }
       end
     end
 
   end
   
   def show
-    @dyna_model = DynaModel.find(params[:id])
-    respond_with(@dyna_models)
+    respond_with(@proxy_dyna_model)
   end
 
   def destroy
-    @dyna_model = DynaModel.find(params[:id])
-    @dyna_model.destroy
-    respond_with(@dyna_model, :location => dyna_models_path)
+    @proxy_dyna_model.destroy
+    respond_with(@proxy_dyna_model, :location => proxy_dyna_models_path)
 
+  end
+
+  private
+  
+  def determine_models
+    @proxy_dyna_model = ProxyDynaModel.find(params[:id])
+    if @proxy_dyna_model.measurement
+      @measurement = @proxy_dyna_model.measurement
+      @experiment = @measurement.experiment
+    elsif @proxy_dyna_model.experiment
+      @experiment = @proxy_dyna_model.experiment
+    end
+    
+    @model = @experiment.model
   end
 
 end
