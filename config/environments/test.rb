2c3d77984d52ad612a33c0ef6da47bc28a41b866
Andre Verissimo <afsverissimo@gmail.com>
2013-10-03 01:20:56 +0100

    test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,8 @@ BacteriaGrowth::Application.configure do
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
+  
+  config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
