6e1a511dd4657e4a12f35893a9b9c8f70cecd720
Andre Verissimo <afsverissimo@gmail.com>
2013-10-03 01:20:13 +0100

    adds first test to user

--- spec/features/user_managements_spec.rb
+++ spec/features/user_managements_spec.rb
@@ -17,7 +17,7 @@ feature "Signing in" do
 
   given(:other_user) { User.new(:email => 'other@example.com', :password => 'rous') }
 
-  scenario "Signing in as another user" do
+  scenario "Signing in as another user with incorrect credentials" do
     visit new_user_session_path
     fill_in 'Email', :with => other_user.email
     fill_in 'Password', :with => other_user.password
@@ -25,4 +25,46 @@ feature "Signing in" do
     expect(page).to have_content 'Invalid email or password'
   end
 
+end
+
+feature "Register user" do
+  
+  given(:registered_user) { User.new(:email => 'user@example.com', :password => 'caplin') }
+  
+  background do
+      u = User.new(:email => registered_user.email, :password => registered_user.password)
+      u.save
+  end
+  
+  scenario "Registering an existing user" do
+    visit new_user_registration_path
+    fill_in 'Email', :with => registered_user.email
+    fill_in 'Password', :with => 'different password'
+    fill_in 'Password confirmation', :with => 'different password'
+    click_button 'Login'
+    expect(page).to have_content 'The email has already been registered.'
+  end
+  
+  given(:new_user) { User.new(:email => 'new@example.com', :password => 'rous') }
+  
+  scenario "Registering a new user with all required fields" do
+    visit new_user_registration_path
+    fill_in 'Email', :with => new_user.email
+    fill_in 'Password', :with => new_user.password
+    fill_in 'Password confirmation', :with => new_user.password
+    click_button 'Login'
+    expect(page).to have_content 'success'
+  end
+  
+  given(:other_user) { User.new(:email => 'other@example.com', :password => 'rous') }
+  
+  scenario "Registering a new user with different passwords" do
+    visit new_user_registration_path
+    fill_in 'Email', :with => other_user.email
+    fill_in 'Password', :with => other_user.password
+    fill_in 'Password confirmation', :with => other_user.email + " different"
+    click_button 'Login'
+    expect(page).to have_content 'don\'t match'
+  end
+  
 end
