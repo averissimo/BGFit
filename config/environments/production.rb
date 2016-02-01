0b1f29b186887769933be3272a4c144eef129165
André Veríssimo <afsverissimo@gmail.com>
2016-02-01 15:53:17 +0000

    updates due to domain change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,5 +62,5 @@ BacteriaGrowth::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'oracle.inesc-id.pt' }
+  config.action_mailer.default_url_options = { :host => 'sels.tecnico.ulisboa.pt' }
 end
